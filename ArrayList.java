6b0036b18d7d0773e37f7b79c505b7eb9f03ac6f
Ammarkb <47086176+Ammarkb@users.noreply.github.com>
2020-09-03 13:15:33 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		ArrayList<String> color = new ArrayList<String>();
+		color.add("Red");
+		color.add("Yellow");
+		color.add("Purple");
+		color.add("Green");
+		System.out.println(color.size());
+		System.out.println(color);
+		
+		Iterator<String> it = color.iterator();
+		while(it.hasNext()){
+		    String s = (String)it.next();
+		    System.out.println(s);
+		}
+	
+	}
+}
