0f2daf99e9c5169379bff51777a8eb4a35eb8fec
Ammarkb <47086176+Ammarkb@users.noreply.github.com>
2020-09-11 20:54:45 +0530

    Create permutation_STL.cpp

---
+++ permutation_STL.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>  
+#include <iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+    int arr[] = { 1, 2, 3 }; 
+    sort(arr, arr + 3); 
+    cout << "The possible permutations are\n"; 
+    do { 
+        cout << arr[0] << " " << arr[1] << " " << arr[2] << "\n"; 
+    } while (next_permutation(arr, arr + 3)); 
+    return 0; 
+}
