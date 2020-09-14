c24a697fe417b3dc3f3e2956a73a4e4b2c6d4a12
Ammarkb <47086176+Ammarkb@users.noreply.github.com>
2020-09-14 20:07:12 +0530

    Create Ascii.cpp

---
+++ Ascii.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char s;
+  cout<<"Enter the character: ";
+	cin >> s;
+	cout << "The numberic(ASCII) value of character"<<ch<<" is "<< int(s); 
+}
