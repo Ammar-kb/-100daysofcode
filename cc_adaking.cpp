8bbeaf44c29de157d9330c0b5a8663ec5358bfc5
Ammarkb <47086176+Ammarkb@users.noreply.github.com>
2020-09-18 22:51:51 +0530

    Create cc_adaking.cpp

---
+++ cc_adaking.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+ 
+int main()
+{
+	int t , n , k , rem;
+	cin>>t;
+	while(t--)
+	{
+		cin>>k;
+		rem = 64 - k;
+ 
+		for(int i=1;i<=8;i++)
+		{
+			for(int j=1;j<=8;j++)
+			{
+				if(i == 8 && j == 8)
+				cout<<"O";
+				else
+				if(rem) cout<<"X" , rem--;
+				else	cout<<".";
+			}
+			cout<<endl;
+		}
+		cout<<endl; 
+	}
+}
