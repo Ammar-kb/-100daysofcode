ac7d7c1a1b391dfb0e0970d5079f91bf3797cfc8
Ammarkb <47086176+Ammarkb@users.noreply.github.com>
2020-10-10 19:09:18 +0530

    Create (Bear and Brother)cf3.cpp

---
+++ (Bear and Brother)cf3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m,count=0;
+    cin>>n>>m;
+    do
+    {
+        n=n*3;
+        m=m*2;
+        count++;
+    }while(n<=m);
+    cout<<count;
+}
