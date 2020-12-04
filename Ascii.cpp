9157ae3973fd70b2e34a9e952ab5aead14f4dcca
Ammarkb <47086176+Ammarkb@users.noreply.github.com>
2020-12-04 22:35:07 +0530

    Update Ascii.cpp

--- Ascii.cpp
+++ Ascii.cpp
@@ -6,5 +6,5 @@ int main()
 	char s;
   cout<<"Enter the character: ";
 	cin >> s;
-	cout << "The numberic(ASCII) value of character"<<ch<<" is "<< int(s); 
+	cout << "The numberic(ASCII) value of character"<<ch<<" is "<< int(s);     
 }
